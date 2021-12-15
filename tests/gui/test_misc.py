7ed936c38e4ca17713aa705a406e7087d3a0797e
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2021-12-15 23:40:49 +1100

    Ensure SecureQLabel behavior (not implementation) is tested

--- tests/gui/test_misc.py
+++ tests/gui/test_misc.py
@@ -1,6 +1,3 @@
-"""
-Tests for the gui helper functions in __init__.py
-"""
 import unittest
 
 from PyQt5.QtCore import QSize, Qt
@@ -10,6 +7,106 @@ from PyQt5.QtWidgets import QApplication
 from securedrop_client.gui import SecureQLabel, SvgLabel, SvgPushButton, SvgToggleButton
 
 app = QApplication([])
+
+LONG_TEXT_202 = "This is a piece of text that is exaclty 202 characters long, lorem ispum dolor sic amet, lorem ipsum dolor sic amet, lorem ispum dolor sic amet, lorem ipsum dolor sic amet, lorem ipsum dolor amet stop."  # noqa: E501
+
+LONG_TEXT_201 = "This is a piece of text that is exactly 200 characters long, lorem ispum dolor sic amet, lorem ipsum dolor sic amet, lorem ispum dolor sic amet, lorem ipsum dolor sic amet, lorem ipsum dolor sic stop."  # noqa: E501
+
+LONG_TEXT_WITH_NEWLINES = (  # noqa: E501
+    "1234567890123456789012345678901234567890123456789012345678901234567890\n" "12345678901"
+)
+
+
+class SecureLabelTest(unittest.TestCase):
+    def test_displays_verbatim_text_by_default(self):
+        label = SecureQLabel("Hello, World!")
+        assert label.text() == "Hello, World!"
+
+    def test_does_not_remove_newlines(self):
+        label = SecureQLabel(LONG_TEXT_WITH_NEWLINES, wordwrap=True)
+        assert label.text() == LONG_TEXT_WITH_NEWLINES
+
+        label = SecureQLabel(LONG_TEXT_WITH_NEWLINES, wordwrap=False)
+        assert label.text() == LONG_TEXT_WITH_NEWLINES
+
+    def test_does_preserve_markup_but_does_not_render_it(self):
+        markup = '<script>alert("hi!");</script>'
+        label = SecureQLabel(markup)
+        assert label.text() == markup
+        assert label.textFormat() == Qt.PlainText
+
+    def test_displays_elision_when_verbatim_exceeds_max_length(self):
+        label = SecureQLabel("Hello, World!", max_length=60)
+        assert label.text() == "Hello,..."
+
+    def test_does_not_display_elision_when_verbatim_is_shorter_max_length(self):
+        label = SecureQLabel("Hello, World!", max_length=600)
+        assert label.text() == "Hello, World!"
+
+    def test_does_not_report_elision_by_default(self):
+        label = SecureQLabel("Hello, World!")
+        assert not label.is_elided()
+
+    def test_reports_elision_when_relevant(self):
+        label = SecureQLabel("Hello, World!", max_length=6)
+        assert label.is_elided()
+
+    def test_only_allows_one_line_of_elided_text(self):
+        label = SecureQLabel("Hello, World!\nHow are you?")
+        assert label.text() == "Hello, World!\nHow are you?"
+
+        label = SecureQLabel("Hello, World!\nHow are you?", max_length=60)
+        assert label.text() == "Hello,..."
+
+        label = SecureQLabel("Hello, World!\nHow are you?", max_length=600)
+        assert label.text() == "Hello, World!"
+
+    def test_does_not_escape_quotes_for_readability(self):
+        label = SecureQLabel("'hello'")
+        assert label.text() == "'hello'"
+
+    def test_trims_leading_and_trailing_whitespace(self):
+        label = SecureQLabel(" \n  Hello, World! \n\t \r ")
+        assert label.text() == "Hello, World!"
+
+    def test_does_not_set_any_tooltip_by_default(self):
+        label = SecureQLabel("Hello, World!\nHow are you?")
+        assert label.toolTip() == ""
+
+    def test_only_sets_a_tooltip_when_elided(self):
+        label = SecureQLabel("Hello, World!", with_tooltip=True, max_length=60)
+        assert label.toolTip() == "Hello, World!"
+
+        label = SecureQLabel("Hello, World!", with_tooltip=True)
+        assert label.toolTip() == ""
+
+    def test_does_not_elide_the_tooltip(self):
+        label = SecureQLabel("Hello, World!\nHow are you?", with_tooltip=True, max_length=60)
+        assert label.toolTip() == "Hello, World!\nHow are you?"
+
+    def test_does_not_truncate_the_tooltip(self):
+        label = SecureQLabel(LONG_TEXT_202, with_tooltip=True, max_length=60)
+        assert label.toolTip() == LONG_TEXT_202
+
+    def test_allows_multiline_tooltips(self):
+        label = SecureQLabel("Hello, World!\nHow are you?", with_tooltip=True, max_length=60)
+        assert label.toolTip() == "Hello, World!\nHow are you?"
+
+    def test_refresh_preview_text_updates_elision(self):
+        label = SecureQLabel("Hello, World!", max_length=32)
+        assert label.text() == "He..."
+        label.max_length = 60
+        label.refresh_preview_text()
+        assert label.text() == "Hello,..."
+
+    def test_refresh_preview_text_truncates_text(self):
+        label = SecureQLabel(LONG_TEXT_201)
+        label.refresh_preview_text()
+        assert label.text() == LONG_TEXT_201
+
+        label = SecureQLabel(LONG_TEXT_202)
+        label.refresh_preview_text()
+        assert label.text() == LONG_TEXT_202[:-1]
 
 
 class SvgLabelTest(unittest.TestCase):
@@ -88,87 +185,3 @@ class SvgToggleButtonTest(unittest.TestCase):
         )
         button.set_icon(on="printer.svg", off="printer.svg", svg_size=QSize(400, 300))
         assert button.icon().actualSize(QSize(1000, 1000)) == QSize(400, 300)
-
-
-def test_SecureQLabel_init():
-    label_text = '<script>alert("hi!");</script>'
-    sl = SecureQLabel(label_text)
-    assert sl.text() == label_text
-
-
-def test_SecureQLabel_init_wordwrap(mocker):
-    """
-    Regression test to make sure we don't remove newlines.
-    """
-    long_string = (
-        "1234567890123456789012345678901234567890123456789012345678901234567890\n" "12345678901"
-    )
-    sl = SecureQLabel(long_string, wordwrap=False)
-    assert sl.text() == long_string
-
-
-def test_SecureQLabel_init_no_wordwrap(mocker):
-    long_string = (
-        "1234567890123456789012345678901234567890123456789012345678901234567890\n" "12345678901"
-    )
-    sl = SecureQLabel(long_string, wordwrap=False)
-    assert sl.text() == long_string
-
-
-def test_SecureQLabel_setText(mocker):
-    sl = SecureQLabel("hello")
-    assert sl.text() == "hello"
-
-    label_text = '<script>alert("hi!");</script>'
-    sl.setTextFormat = mocker.MagicMock()
-    sl.setText(label_text)
-    assert sl.text() == label_text
-    # Ensure *safe* plain text with no HTML entities.
-    sl.setTextFormat.assert_called_once_with(Qt.PlainText)
-
-
-def test_SecureQLabel_get_elided_text(mocker):
-    # 70 character string
-    long_string = "1234567890123456789012345678901234567890123456789012345678901234567890"
-    sl = SecureQLabel(long_string, wordwrap=False, max_length=100)
-    elided_text = sl.get_elided_text(long_string)
-    assert sl.text() == elided_text
-    assert "..." in elided_text
-
-
-def test_SecureQLabel_get_elided_text_short_string(mocker):
-    # 70 character string
-    long_string = "123456789"
-    sl = SecureQLabel(long_string, wordwrap=False, max_length=100)
-    elided_text = sl.get_elided_text(long_string)
-    assert sl.text() == elided_text
-    assert elided_text == "123456789"
-
-
-def test_SecureQLabel_get_elided_text_only_returns_oneline(mocker):
-    # 70 character string
-    string_with_newline = "this is a string\n with a newline"
-    sl = SecureQLabel(string_with_newline, wordwrap=False, max_length=100)
-    elided_text = sl.get_elided_text(string_with_newline)
-    assert sl.text() == elided_text
-    assert elided_text == "this is a string"
-
-
-def test_SecureQLabel_get_elided_text_only_returns_oneline_elided(mocker):
-    # 70 character string
-    string_with_newline = "this is a string\n with a newline"
-    sl = SecureQLabel(string_with_newline, wordwrap=False, max_length=38)
-    elided_text = sl.get_elided_text(string_with_newline)
-    assert sl.text() == elided_text
-    assert "..." in elided_text
-
-
-def test_SecureQLabel_quotes_not_escaped_for_readability():
-    sl = SecureQLabel("'hello'")
-    assert sl.text() == "'hello'"
-
-
-def test_SecureQLabel_trims_leading_and_trailing_whitespace():
-    string_with_whitespace = "\n \n this is a string with leading and trailing whitespace \n"
-    sl = SecureQLabel(string_with_whitespace)
-    assert sl.text() == "this is a string with leading and trailing whitespace"
