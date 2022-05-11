3ea99494db4fc68be49c93bbda2d646ba8d320d5
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2022-05-11 22:05:06 +1000

    Fix name conflict in export module

--- securedrop_client/export/__init__.py
+++ securedrop_client/export/__init__.py
@@ -1,2 +1,4 @@
 # The GUI can rely on these classes without knowing about any other class from this directory.
+from securedrop_client.legacy_export import Export, ExportError, ExportStatus  # noqa: F401
+
 from .device import ConnectedDevice, EncryptedDevice  # noqa: F401
